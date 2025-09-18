42ed227cbabe5f5df096d4ec85632e11115622a2
Ekbal <78045@student.dorset-college.ie>
2025-09-18 13:46:54 +0100

    car

--- Program.cs
+++ Program.cs
@@ -1,28 +1,34 @@
+using FleetManager.Data;
+using fs_a_2025_blazor_0918_78045.Repositories;
 using fs_a_2025_blazor_0918_78045.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped<CarRepository>();
+// Add services to the container.  
+builder.Services.AddRazorComponents()
+   .AddInteractiveServerComponents();
+
+builder.Services.AddRazorComponents();
+builder.Services.AddScoped<CarRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+   .AddInteractiveServerRenderMode();
 
 app.Run();
